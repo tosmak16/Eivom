a693549e0308d9587b8c0c6ce62eb72a04442302
Tosmak <tosmak16@gmail.com>
2019-03-16 22:59:48 +0100

    update deployment script

--- .circleci/deploy-staging.sh
+++ .circleci/deploy-staging.sh
@@ -1,3 +1,3 @@
 sh ./.circleci/install-aws.sh
 npm run build
-aws s3 sync ./dist s3://eivom.app.staging.com/ --delete --exclude '.git/*'
+aws s3 sync ./dist/eivom s3://eivom.app.staging.com/ --delete --exclude '.git/*'
